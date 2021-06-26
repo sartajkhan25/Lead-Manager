6ac0ca21d8f8e29486e6556f0835c377540df169
Sartaj Khan <sartaj2493@gmail.com>
2021-06-26 07:11:55 +0530

    lead-Package

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import store from "./store";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import News from "./components/testing/News";
 import IndiaMart from "./components/indiamart/IndiaMart";
-import Calender from "./components/calender/Calender";
-import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
-// App
+
 function App() {
   return (
     <Provider store={store}>
@@ -19,27 +17,13 @@ function App() {
             <Link to="/im" className="links">
               IndiaMart
             </Link>
-            <Link to="/calender" className="links">
-              Client Calender
-            </Link>
-            <Link to="/appliedjobs" className="links">
-              Applied Jobs
-            </Link>
-            <Link to="/project4" className="links">
-              Project 4
-            </Link>
-            <Link to="/project5" className="links">
-              Project 5
-            </Link>
+
           </div>
           <Switch>
             
             <div className='mainBox'>
               <Route exact path="/" component={News}></Route>
               <Route exact path="/im" component={IndiaMart}></Route>
-              <Route exact path="/calender" component={Calender}></Route>
-              <Route exact path="/appliedjobs" component={AppliedJobs}></Route>
-              <Route exact path="/project5" component={News}></Route>
             </div>
           </Switch>
         </div>
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,8 +1,6 @@
-import addCompReducer from "./redux/jobApplied/reducers";
 import fetchReducer from "./redux/reducers";
 import { combineReducers } from "redux";
 
 export const rootReducer = combineReducers({
-    company:addCompReducer,
     insiamart:fetchReducer
 })
